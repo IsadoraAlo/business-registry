c11ce6caad50612fdb66d0111ac3cb2030997f9d
Isadora <rm84246@fiap.com.br>
2023-05-24 00:04:16 -0300

    Criando um menu

--- business-registry-web/src/app/app.module.ts
+++ business-registry-web/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CadastrosModule } from './features/commom/cadastros/cadastros.module';
+import { MenuComponent } from './features/commom/home/menu/menu.component';
+import { AboutComponent } from './features/commom/home/pages/about/about.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MenuComponent, AboutComponent],
   imports: [BrowserModule, FormsModule, CadastrosModule],
   providers: [],
   bootstrap: [AppComponent],
