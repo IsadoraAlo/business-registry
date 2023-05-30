687ab49edb2fab82d06c3f38e3aae24f63fbc2e7
Isadora <rm84246@fiap.com.br>
2023-05-30 16:02:35 -0300

    about page

--- business-registry-web/src/app/app.module.ts
+++ business-registry-web/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { CadastrosModule } from './features/commom/cadastros/cadastros.module';
 import { MenuComponent } from './features/commom/home/menu/menu.component';
 import { AboutComponent } from './features/commom/home/pages/about/about.component';
+import { FooterComponent } from './features/commom/home/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent, AboutComponent],
+  declarations: [AppComponent, MenuComponent, AboutComponent, FooterComponent],
   imports: [BrowserModule, FormsModule, CadastrosModule],
   providers: [],
   bootstrap: [AppComponent],
