0958e796fc0a1b98bf1665c8e0e01ea0787f3f24
Isadora <rm84246@fiap.com.br>
2023-06-19 09:33:32 -0300

    Separando componentes

--- business-registry-web/src/app/app.module.ts
+++ business-registry-web/src/app/app.module.ts
@@ -4,9 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CadastrosModule } from './features/commom/cadastros/cadastros.module';
-import { MenuComponent } from './features/commom/home/menu/menu.component';
+import { MenuComponent } from './features/componentes/menu/menu.component';
 import { AboutComponent } from './features/commom/home/pages/about/about.component';
-import { FooterComponent } from './features/commom/home/footer/footer.component';
+import { FooterComponent } from './features/componentes/footer/footer.component';
 import { IndexComponent } from './features/commom/home/pages/index/index.component';
 import { AppRoutingModule } from './app-routing.module';
 
