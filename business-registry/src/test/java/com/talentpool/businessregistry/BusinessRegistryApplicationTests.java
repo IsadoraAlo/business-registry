b713cfc4c3878dc7f541f91f90bfa967c67d6382
Isadora <rm84246@fiap.com.br>
2023-07-24 19:49:47 -0300

    criando o projeto back-end

---
+++ business-registry/src/test/java/com/talentpool/businessregistry/BusinessRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talentpool.businessregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
