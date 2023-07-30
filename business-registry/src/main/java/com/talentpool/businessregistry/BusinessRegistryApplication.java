a6817c2a9d85ac99fbe26ec0b4cf9c5f2e89e02e
Isadora <rm84246@fiap.com.br>
2023-07-30 10:02:05 -0300

    Criando projeto back-end com properties

---
+++ business-registry/src/main/java/com/talentpool/businessregistry/BusinessRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.talentpool.businessregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessRegistryApplication.class, args);
+	}
+
+}
