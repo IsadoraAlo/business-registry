3b21de1267edaf7d206b5e1b5dae7f58df5d9556
Isadora <rm84246@fiap.com.br>
2023-07-31 13:06:55 -0300

    Controller de Usuário

---
+++ business-registry/src/main/java/com/talentpool/businessregistry/controller/usuario/UsuarioController.java
@@ -0,0 +1,70 @@
+package com.talentpool.businessregistry.controller.usuario;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.talentpool.businessregistry.exception.ResourceNotFoundException;
+import com.talentpool.businessregistry.model.usuario.Usuario;
+import com.talentpool.businessregistry.repository.usuario.UsuarioRepository;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api/v1/")
+public class UsuarioController {
+	
+	private static final String MESSAGE_ERROR = "Não existe usuário com o id ";
+
+	@Autowired
+	UsuarioRepository usuarioRepository;
+
+	@GetMapping("/usuarios")
+	public List<Usuario> getAllUsuarios() {
+		return usuarioRepository.findAll();
+	}
+
+	@PostMapping("/usuarios")
+	public Usuario createUsuario(@RequestBody Usuario usuario) {
+		return usuarioRepository.save(usuario);
+	}
+
+	@GetMapping("/usuarios/{id}")
+	public ResponseEntity<Usuario> getUsuarioById(@PathVariable Long id) {
+		Usuario usuario = usuarioRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(MESSAGE_ERROR + id));
+		return ResponseEntity.ok(usuario);
+	}
+
+	@PutMapping("/usuarios/{id}")
+	public ResponseEntity<Usuario> updateUsuario(@PathVariable Long id, @RequestBody Usuario usuarioDetails) {
+		Usuario usuario = usuarioRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(MESSAGE_ERROR + id));
+		usuario.setCelular(usuarioDetails.getCelular());
+		usuario.setDocumento(usuarioDetails.getDocumento());
+		usuario.setEmail(usuarioDetails.getEmail());
+		usuario.setEnderecos(usuarioDetails.getEnderecos());
+		usuario.setNome(usuarioDetails.getNome());
+		usuario.setSenha(usuarioDetails.getSenha());
+		Usuario updatedEmployee = usuarioRepository.save(usuario);
+		return ResponseEntity.ok(updatedEmployee);
+	}
+
+	@DeleteMapping("/usuarios/{id}")
+	public ResponseEntity<Map<String, Boolean>> deleteUsuario(@PathVariable Long id) {
+		Usuario usuario = usuarioRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(MESSAGE_ERROR + id));
+		usuarioRepository.delete(usuario);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return ResponseEntity.ok(response);
+	}
+}
