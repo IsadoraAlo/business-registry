d1aaa8d08c46a77a32adf8a6d6025a0de2533abb
Isadora <rm84246@fiap.com.br>
2023-08-01 22:13:14 -0300

    Criação de usuário - candidato

--- business-registry-web/src/app/features/candidatos/candidatos.module.ts
+++ business-registry-web/src/app/features/candidatos/candidatos.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from 'src/app/app-routing.module';
-import { BoasVindasCandidatoComponent } from './pages/boas-vindas-candidato/boas-vindas-candidato.component';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 import { ComponentesModule } from '../commom/componentes/componentes.module';
+import { MenuCandidatoComponent } from './componentes/menu-candidato/menu-candidato.component';
+import { BoasVindasCandidatoComponent } from './pages/boas-vindas-candidato/boas-vindas-candidato.component';
 import { CandidatosBuscaVagasComponent } from './pages/candidatos-busca-vagas/candidatos-busca-vagas.component';
 import { CandidatosCadastroComponent } from './pages/candidatos-cadastro/candidatos-cadastro.component';
-import { MenuCandidatoComponent } from './componentes/menu-candidato/menu-candidato.component';
 @NgModule({
   declarations: [CandidatosCadastroComponent, MenuCandidatoComponent, BoasVindasCandidatoComponent, CandidatosBuscaVagasComponent],
-  imports: [CommonModule, AppRoutingModule, ComponentesModule],
-  exports: [CandidatosCadastroComponent, BoasVindasCandidatoComponent]
+  imports: [CommonModule, AppRoutingModule, ComponentesModule, FormsModule, HttpClientModule],
+  exports: [CandidatosCadastroComponent, BoasVindasCandidatoComponent],
 })
 export class CandidatosModule { }
--- business-registry-web/src/app/features/candidatos/pages/candidatos-cadastro/candidatos-cadastro.component.ts
+++ business-registry-web/src/app/features/candidatos/pages/candidatos-cadastro/candidatos-cadastro.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from 'src/app/utils/models/usuario/usuario.model';
+import { UsuarioService } from 'src/app/utils/services/usuario/usuario.service';
 
 @Component({
   selector: 'app-candidatos-cadastro',
@@ -6,4 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./candidatos-cadastro.component.scss']
 })
 export class CandidatosCadastroComponent {
+  usuario: Usuario = new Usuario();
+
+  constructor(private usuarioService: UsuarioService,
+    private router: Router) { }
+
+  saveUsuario() {
+    this.usuario.tipo = 'CANDIDATO';
+    this.usuario.status = true;
+    this.usuarioService.criarUsuario(this.usuario).subscribe(usuario => {
+      console.log(usuario);
+      this.router.navigate(['/login']);
+    },
+      error => console.log(error));
+  }
+
+  onSubmit() {
+    console.log(this.usuario);
+    this.saveUsuario();
+  }
 }
